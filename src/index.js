666f83fb707b4e95b3f48bb319ff04623b194d6e
Jhonatan Morais <jhonatanvinicius@gmail.com>
2021-06-20 20:28:01 -0300

    fix beauty

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './pages/assets/index.css';
 import Template from './pages/template';
 
-if (process.env.NODE_ENV === "development") {
-    // You can't use import in a conditional so we're using require() so no
-    // Mirage JS code will ever reach your production build.
-    require('./miragejs/server').makeServer();
+if (process.env.NODE_ENV === 'development') {
+  // You can't use import in a conditional so we're using require() so no
+  // Mirage JS code will ever reach your production build.
+  require('./miragejs/server').makeServer();
 }
 
-
-ReactDOM.render(<Template />,
-  document.getElementById('root')
-);
-
-
+ReactDOM.render(<Template />, document.getElementById('root'));
