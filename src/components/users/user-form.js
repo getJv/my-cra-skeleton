9b094215841b57a0ace518d53ef6b9f55f15b26f
Jhonatan Morais <jhonatanvinicius@gmail.com>
2021-06-21 20:47:17 -0300

    add more test

--- src/components/users/user-form.js
+++ src/components/users/user-form.js
@@ -3,14 +3,27 @@ import React from 'react';
 function UserForm({ handleSubmit, handleCancel, handleChange, formData }) {
   return (
     <form name="user-form" onSubmit={handleSubmit}>
-      <label>
-        <p>Name</p>
-        <input type="text" name="name" onChange={handleChange} value={formData.name || ''} />
-      </label>
-      <label>
-        <p>Mobile</p>
-        <input type="text" name="mobile" onChange={handleChange} value={formData.mobile || ''} />
-      </label>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          value={formData.name || ''}
+        />
+      </div>
+      <div>
+        <label htmlFor="mobile">Mobile</label>
+        <input
+          id="mobile"
+          name="mobile"
+          type="text"
+          onChange={handleChange}
+          value={formData.mobile || ''}
+        />
+      </div>
+
       <div>
         <button type="submit" data-testid="submit-button">
           Save
--- src/components/users/user-form.unit.spec.js
+++ src/components/users/user-form.unit.spec.js
@@ -3,14 +3,14 @@ import userEvent from '@testing-library/user-event';
 import React from 'react';
 import UserForm from './user-form';
 
-const users = { id: 1, name: 'first name', mobile: '44444' };
+const user = { id: 1, name: 'first name', mobile: '44444' };
 const handleSubmit = jest.fn(e => e.preventDefault());
 const handleChange = jest.fn();
 const handleCancel = jest.fn();
 const renderUserForm = () => {
   render(
     <UserForm
-      formData={users}
+      formData={user}
       handleChange={handleChange}
       handleSubmit={handleSubmit}
       handleCancel={handleCancel}
@@ -18,10 +18,40 @@ const renderUserForm = () => {
   );
 };
 
-describe('UserList', () => {
+describe('UserForm', () => {
   it('should render the component', () => {
     renderUserForm();
     expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+  it('should render the name input', () => {
+    renderUserForm();
+    const nameField = screen.getByLabelText('Name');
+    expect(nameField).toBeInTheDocument();
+    expect(nameField).toHaveProperty('type', 'text');
+    expect(nameField).toHaveProperty('value', user.name);
+  });
+  it('should render inputs empty when formData is empty', () => {
+    const emptyFormData = {};
+    render(
+      <UserForm
+        formData={emptyFormData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleCancel={handleCancel}
+      />,
+    );
+    const nameField = screen.getByLabelText('Name');
+    const mobileField = screen.getByLabelText('Mobile');
+
+    expect(nameField).toHaveProperty('value', '');
+    expect(mobileField).toHaveProperty('value', '');
+  });
+  it('should render the mobile input', () => {
+    renderUserForm();
+    const mobileField = screen.getByLabelText('Mobile');
+    expect(mobileField).toBeInTheDocument();
+    expect(mobileField).toHaveProperty('type', 'text');
+    expect(mobileField).toHaveProperty('value', user.mobile);
   });
   it('should call props.handleCancel when cancel-button is clicked', async () => {
     renderUserForm();
